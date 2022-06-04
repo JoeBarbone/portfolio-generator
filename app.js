d25b452c411717d97f659cf4e1befc9f8313d695
Joseph Barbone <JoeBarbone@aol.com>
2022-06-03 21:05:40 -0400

    9.1.6 from HP Envy

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-var commandLineArgs = process.argv
-console.log(commandLineArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+};
+
+printProfileData(profileDataArgs);
