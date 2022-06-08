e7fb5fe459ffd4b77f8e5d8baf120f1e95213678
Joseph Barbone <JoeBarbone@aol.com>
2022-06-08 11:41:54 -0400

    9.2.7 completed page generation

--- app.js
+++ app.js
@@ -1,33 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-//const printProfileData = profileDataArr => {
-    // for (let i=0; i < profileDataArgs.length; i++) {
-    //     console.log(profileDataArr[i]);
-    // }
+const profileDataArgs = process.argv.slice(2);
 
-//    profileDataArr.forEach(profileItem => console.log("Profile: " + profileItem));
-//};
+const [name, github] = profileDataArgs;
 
-//printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err;
 
-//var sayHello = (name) => {
-//    console.log("Hello! " + name);
-//}
-
-// let is block-scoped, so changing its value in a block does change its value outside the block _if_ the variable is not redeclared in the block:
-
-//let four = 'four: outside the block';
-
-//if (true === true) {
-//  four = 'four: inside the block'; // notice: we don't redeclare the variable
-//  console.log(four); // prints 'four: inside the block'
-//}
-
-// console.log(four); // prints 'four: inside the block', because we didn't redeclare the variable inside the block. That meant we referenced the variable outside the block, and we therefore changed it.
-
-
-const generatePage = () => ("Name: Jane, Github: janehub");
-
-
-console.log(generatePage());
+    console.log("Portfolio Complete! CHeck out index.html to see the output!");
+});
